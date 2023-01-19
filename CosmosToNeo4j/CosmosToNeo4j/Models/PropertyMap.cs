14ec0f46e84a61356bb0f2acf42f349113deb1e3
Charlotte Skardon <charlotte@tournr.com>
2023-01-19 16:22:35 +0000

    Updating Mappings classes to read property definitions in json

---
+++ CosmosToNeo4j/CosmosToNeo4j/Models/PropertyMap.cs
@@ -0,0 +1,9 @@
+﻿namespace CosmosToNeo4j.Models;
+
+public class PropertyMap
+{
+    public string? Cosmos { get; set; }
+    public string? Neo4j { get; set; }
+    public bool Indexed { get; set; }
+    public bool Ignored { get; set; }
+}
