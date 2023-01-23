074632a18455fd02954005e1cf442da9df9d6437
Charlotte Skardon <charlotte@tournr.com>
2023-01-23 16:05:29 +0000

    Allows a user to generate a mappings file from their CosmosDB

--- CosmosToNeo4j/CosmosToNeo4j/Models/Map.cs
+++ CosmosToNeo4j/CosmosToNeo4j/Models/Map.cs
@@ -1,8 +1,11 @@
 ﻿namespace CosmosToNeo4j.Models;
 
+using Newtonsoft.Json;
+
 public class Map
 {
     public string? Cosmos { get; set; }
     public string? Neo4j { get; set; }
+    [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
     public List<PropertyMap>? Properties { get; set; }
 }
